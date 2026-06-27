ee591e6ddf603b6d8e7debd3555599a9bc040c19
Krishnaveni gandi <gandikrishnaveni123@gmail.com>
2026-06-27 19:55:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-=======
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
