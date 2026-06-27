2f8d61ea1b051dbde2ccd6e9d6960f1ae8a222e1
Krishnaveni gandi <gandikrishnaveni123@gmail.com>
2026-06-27 20:01:47 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // Signup
 router.post('/signup', async (req, res) => {
